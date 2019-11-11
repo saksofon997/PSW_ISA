7e90bd88431e567cc1a8e791bebae5019119ee07
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2019-11-11 16:51:09 +0100

    1.1_2-2 frontend: user service, routing guards, header

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuardService } from './guards/auth-guard.service'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+	// {
+	// 	path: '',
+	// 	component: HomeComponent,
+	// 	canActivate: [AuthGuardService]
+	// },
+	// {
+	// 	path: 'admin',
+	// 	component: AdminComponent,
+	// 	canActivate: [AuthGuardService],
+	// 	data: { roles: ["clinicalCenterAdmin, clinicAdmin, doctor, nurse, patient"] }
+	// },
+	// {
+	// 	path: 'login',
+	// 	component: LoginComponent
+	// },
+	// {
+	// 	path: 'register',
+	// 	component: RegisterComponent
+	// },
+	// // otherwise redirect to home
+	// { path: '**', redirectTo: '' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ client/src/app/guards/auth-guard.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, Route } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserService } from '../services/user.service';
+
+
+@Injectable({
+	providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+	constructor(private userService: UserService,
+		private router: Router) { }
+
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+		const currentUser = this.userService.checkLoggedIn();
+        if (currentUser) {
+            // check if route is restricted by role
+            if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
+                // role not authorised so redirect to home page
+                this.router.navigate(['/']);
+                return false;
+            }
+
+            // authorised so return true
+            return true;
+        }
+
+        // not logged in so redirect to login page with the return url
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+	
+}
