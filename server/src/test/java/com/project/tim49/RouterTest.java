a1b069c67f65c86e3d311a811843f37594b807d1
unknown <petrosbasic@gmail.com>
2020-02-05 14:29:53 +0100

    TEST-unit: fix selenium test.

--- server/src/test/java/com/project/tim49/RouterTest.java
+++ server/src/test/java/com/project/tim49/RouterTest.java
@@ -1,24 +1,11 @@
-package selenium;
+package com.project.tim49;
 
 import org.junit.runner.RunWith;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-<<<<<<< Updated upstream:server/src/test/java/selenium/RouterTest.java
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.List;
-=======
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
@@ -29,26 +16,23 @@ import org.junit.Test;
 import selenium.SearchPage;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
->>>>>>> Stashed changes:server/src/test/java/com/project/tim49/RouterTest.java
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.AssertJUnit.assertEquals;
 
-<<<<<<< Updated upstream:server/src/test/java/selenium/RouterTest.java
-@RunWith(SpringJUnit4ClassRunner.class)
-=======
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @TestPropertySource("classpath:application-test.properties")
->>>>>>> Stashed changes:server/src/test/java/com/project/tim49/RouterTest.java
 public class RouterTest {
-
+    @LocalServerPort
+    private int port;
     private WebDriver browser;
 
     private SearchPage searchPage;
 
+    private String base;
 
-    @BeforeMethod
+    @Before
     public void setUp() {
         // instantiate chrome browser
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
@@ -58,12 +42,16 @@ public class RouterTest {
         browser = new ChromeDriver(chromeOptions);
         //maximize window
         browser.manage().window().maximize();
-
+        this.base = "http://localhost:" + port;
+        System.out.println(this.base);
     }
 
     @Test
     public void testPreDefinedAppointment() {
-        browser.navigate().to("http://localhost:8080/#/login");
+        System.out.println(this.base);
+        browser.navigate().to(base+"/#/login");
+        (new WebDriverWait(browser, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
         browser.findElement(By.name("email")).sendKeys("patient1@kcv.rs");
         browser.findElement(By.name("password")).sendKeys("123456");
         browser.findElement(By.cssSelector(".btn-primary")).click();
@@ -92,12 +80,12 @@ public class RouterTest {
 
     @Test
     public void testSearchAndScheduleAppointment() {
-        browser.navigate().to("http://localhost:8080/#/login");
+        browser.navigate().to(base+"/#/login");
         browser.findElement(By.name("email")).sendKeys("patient1@kcv.rs");
         browser.findElement(By.name("password")).sendKeys("123456");
         browser.findElement(By.cssSelector(".btn-primary")).click();
 
-        browser.navigate().to("http://localhost:8080/#/patient");
+        browser.navigate().to(base+"/#/patient");
         (new WebDriverWait(browser, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".btn-light")));
         browser.findElement(By.cssSelector(".btn-light")).click();
@@ -130,7 +118,7 @@ public class RouterTest {
         browser.findElement(By.xpath("//a[@id=\"showDoctor10\"]")).click();
         (new WebDriverWait(browser, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id=\"select13:10\"]")));
-        browser.findElement(By.xpath("//button[@id=\"select12:12\"]")).click();
+        browser.findElement(By.xpath("//button[@id=\"select12:10\"]")).click();
         (new WebDriverWait(browser, 10))
                 .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//select[@name=\"formTOE\"]")));
         browser.findElement(By.xpath("//button[@type=\"submit\"]")).click();
@@ -189,34 +177,37 @@ public class RouterTest {
 
     @Test
     public void testReserveOrdination() {
-        browser.navigate().to("http://localhost:8080/#/login");
+        browser.navigate().to(base+"/#/login");
         browser.findElement(By.name("email")).sendKeys("adminc1@kcv.rs");
         browser.findElement(By.name("password")).sendKeys("123456");
         browser.findElement(By.cssSelector(".btn-primary")).click();
 
-//        (new WebDriverWait(browser, 10))
-//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id=\"dropdown1\"]")));
-//        WebElement optionsButton =  browser.findElement(By.xpath("//button[@id=\"dropdown1\"]"));
-//        optionsButton.click();
-//        WebElement appointmentLink =  browser.findElement(By.xpath("//a[@id=\"appointment1\"]"));
-//        appointmentLink.click();
-//        (new WebDriverWait(browser, 10))
-//                .until(ExpectedConditions.presenceOfElementLocated( By.xpath("//button[@id=\"appointment6\"]")));
-//        browser.findElement(By.xpath("//button[@id=\"appointment6\"]")).click();
-//        try{
-//            waitForAlert(browser);
-//        }catch (InterruptedException e){
-//            System.out.println(e);
-//        }
-//        Alert alert = browser.switchTo().alert();
-//        String alertText = alert.getText();
-//        alert.dismiss();
-//        assertEquals(alertText,
-//                "Successfully selected an appointment");
+        (new WebDriverWait(browser, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@id=\"dropdownMenuButton1\"]")));
+        WebElement optionsButton =  browser.findElement(By.xpath("//button[@id=\"dropdownMenuButton1\"]"));
+        optionsButton.click();
+        browser.findElement(By.xpath("//a[@id=\"selectOrd1\"]")).click();
+        (new WebDriverWait(browser, 10))
+                .until(ExpectedConditions.presenceOfElementLocated( By.xpath("//button[@id=\"showAvailable62\"]")));
+        browser.findElement(By.xpath("//button[@id=\"showAvailable62\"]")).click();
+        (new WebDriverWait(browser, 10))
+                .until(ExpectedConditions.presenceOfElementLocated( By.xpath("//div[@id=\"modal1\"]")));
+        browser.findElement(By.xpath("//button[@class='btn btn-secondary']//self::button[1]")).click();
+        browser.findElement(By.xpath("//button[@id='btnCreateAppointment']")).click();
+        try{
+            waitForAlert(browser);
+        }catch (InterruptedException e){
+            System.out.println(e);
+        }
+        Alert alert = browser.switchTo().alert();
+        String alertText = alert.getText();
+        alert.dismiss();
+        assertEquals(alertText,
+                "Appointment succesfully created");
 
     }
 
-    @AfterMethod
+    @After
     public void tearDown() {
         browser.close();
     }
