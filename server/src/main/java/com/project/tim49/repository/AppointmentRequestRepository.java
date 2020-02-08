31f59f8a180497409ae8f70c7454ce30af04a969
mihajlo-perendija <54019549+mihajlo-perendija@users.noreply.github.com>
2020-02-08 04:53:50 +0100

    Patient_appointment_request_confirmation: cron job now transactional, front calendar changes

--- server/src/main/java/com/project/tim49/repository/AppointmentRequestRepository.java
+++ server/src/main/java/com/project/tim49/repository/AppointmentRequestRepository.java
@@ -3,8 +3,12 @@ package com.project.tim49.repository;
 import com.project.tim49.model.AppointmentRequest;
 import com.project.tim49.model.Clinic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +16,8 @@ public interface AppointmentRequestRepository extends JpaRepository<AppointmentR
 
     ArrayList<AppointmentRequest> getByClinicAndApprovedFalse(Clinic clinic);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({@QueryHint(name = "javax.persistence.lock.timeout", value ="0")})
     ArrayList<AppointmentRequest> getAllByApprovedFalse();
 
     @Query("select request from AppointmentRequest request where " +
